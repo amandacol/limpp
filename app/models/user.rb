160dcc4d3833c2f41d8840a3a858564f84a4f140
Amanda Bezerra <colmenero88@bol.com.br>
2020-03-31 16:32:24 -0300

    installing google log in gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :omniauthable, omniauth_providers: [:facebook]
+  devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 

a2cefddc6829805c2a97e99e074f5a6389f87816
Amanda Bezerra <colmenero88@bol.com.br>
2020-03-31 16:44:59 -0300

    fixing omniauth archive

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-RRails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.secrets.google_client_id, Rails.application.secrets.google_client_secret
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end

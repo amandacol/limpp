65ae695c164460ed39abb92ad0a2e7b7bf2551f2
Amanda Bezerra <colmenero88@bol.com.br>
2020-04-01 18:10:35 -0300

    including pgsearch gem

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  include PgSearch::Model
+  has_many :reviews
+  pg_search_scope :search_by_name_and_ingredient,
+    against: [ :name, :ingredient ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end

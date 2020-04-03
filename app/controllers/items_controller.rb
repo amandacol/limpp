a66653b8b25a8a6596fb27486a6181c2e5b86051
Amanda Bezerra <colmenero88@bol.com.br>
2020-04-03 16:37:40 -0300

    filling items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,57 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  def new
+    @item = Item.new
+    authorize @item
+  end
+
+  def create
+    @item = Item.new(item_params)
+    authorize @item
+    @item.user = current_user
+
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+
+  end
+
+  def index
+    if params[:query].present?
+      @items = policy_scope(Item).search_by_name_and_ingredient(params[:query])
+    else
+      @items = Item.all
+    end
+    authorize @item
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+  end
+  end
+
+
+  def destroy
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+    authorize @item
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :ingredient, :certification, :coupon)
+  end
 end

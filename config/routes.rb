3f6e327593a1b1d1071bcfbed4fcdde03e554459
Amanda Bezerra <colmenero88@bol.com.br>
2020-04-27 15:22:31 -0300

    new sendtous page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   root to: 'pages#home'
   get '/first' => 'pages#first'
+  get '/sendtous' => 'pages#sendtous'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :items do

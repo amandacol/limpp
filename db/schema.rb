3e0160d4ddb30f91a4614fba5ce3b8d3539e94ad
Amanda Bezerra <colmenero88@bol.com.br>
2020-05-07 16:43:13 -0300

    starting the filtering process

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_221357) do
+ActiveRecord::Schema.define(version: 2020_05_07_182956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_04_27_221357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "category"
+    t.string "subcategory"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 

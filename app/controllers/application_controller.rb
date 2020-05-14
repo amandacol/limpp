9b21c56eeb4fe62b445b20b51a7b33d7f9fee6b8
Amanda Bezerra <colmenero88@bol.com.br>
2020-05-14 15:53:17 -0300

    fixing the pundit problem for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
+  protect_from_forgery
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?

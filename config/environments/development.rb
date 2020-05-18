f0b9498715cafcc7c1acf6b0655f3a475b2996ee
Amanda Bezerra <colmenero88@bol.com.br>
2020-05-18 16:44:04 -0300

    finishing the process of action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,8 @@ Rails.application.configure do
 
    # Mail configuration
 
-
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.default_url_options = { host: "http://localhost:3000/" }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true

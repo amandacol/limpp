3c4dea0aa386570c104061098beb89cc684d8fda
Amanda Bezerra <colmenero88@bol.com.br>
2020-05-25 19:32:26 -0300

    changing style of show details

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -2,6 +2,7 @@ import "jquery-bar-rating";
 import $ from 'jquery';
 
 const initStarRating = () => {
+console.log($("#review_stars"))
   $('#review_stars').barrating({
     theme: 'css-stars'
   });

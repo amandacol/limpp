8c6dbac55e3a5f8e307b5c097ae9f37517829a73
Amanda Bezerra <colmenero88@bol.com.br>
2020-06-02 16:26:35 -0300

    eliminating coupon from item columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_203701) do
+ActiveRecord::Schema.define(version: 2020_06_02_174332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2020_06_01_203701) do
     t.string "name"
     t.string "ingredient"
     t.string "certification"
-    t.string "coupon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"

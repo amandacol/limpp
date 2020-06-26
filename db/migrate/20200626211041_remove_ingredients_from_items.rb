0fc3d74897e553ed6709cb8609a7cda93620fb11
Amanda Bezerra <colmenero88@bol.com.br>
2020-06-26 19:20:32 -0300

    non succesfully remove ingredient colum

---
+++ db/migrate/20200626211041_remove_ingredients_from_items.rb
@@ -0,0 +1,5 @@
+class RemoveIngredientsFromItems < ActiveRecord::Migration[5.2]
+  def change
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_133414) do
+ActiveRecord::Schema.define(version: 2020_06_26_211041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,11 +65,11 @@ ActiveRecord::Schema.define(version: 2020_06_23_133414) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.string "ingredient"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "average", default: 0
+    t.string "ingredient"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 

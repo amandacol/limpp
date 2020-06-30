dd80528fdd11535e0dde75d40313d4fe04a80c1d
Amanda Bezerra <colmenero88@bol.com.br>
2020-06-30 15:44:23 -0300

    fixing all oaumniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,26 +1,28 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
-
-    if user.persisted?
-      sign_in_and_redirect user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    else
-      session['devise.facebook_data'] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
-    end
+  @user = User.create_from_provider_data(request.env['omniauth.auth'])
+  if @user.persisted?
+    sign_in_and_redirect @user
+    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+  else
+    flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
+    redirect_to new_user_registration_url
   end
+end
 
-  def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      user = User.from_omniauth(request.env['omniauth.auth'])
-
-      if user.persisted?
-        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-        sign_in_and_redirect user, event: :authentication
-      else
-        session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url
-      end
+def google_oauth2
+  @user = User.create_from_google_data(request.env['omniauth.auth'])
+  if @user.persisted?
+    sign_in_and_redirect @user
+    set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+  else
+    flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+    redirect_to new_user_registration_url
   end
+end
+
+def failure
+  flash[:error] = 'Tivemos um problema com sua autenticação. Por favor, registre-se na nossa plataforma, ou tente novamente mais tarde.'
+  redirect_to new_user_registration_url
+end
 end

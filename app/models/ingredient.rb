c0b19cc1c612d2de05528fa945a80799c7452968
Amanda Bezerra <colmenero88@bol.com.br>
2020-10-06 13:23:55 -0300

    updating item toxicity rate when updating ingredient automaticaly

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ApplicationRecord
+  after_update :update_toxicity
   has_many :combinations, dependent: :destroy
   has_many :mergers, dependent: :destroy
   has_many :articles, dependent: :destroy
@@ -8,4 +9,11 @@ class Ingredient < ApplicationRecord
       using: {
         tsearch: { prefix: true }
       }
+
+  def update_toxicity
+    self.combinations.each do |combination|
+      combination.item.toxicity_classifier
+    end
+  end
+
 end

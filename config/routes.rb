0d2d5fa8aca0b7a3e9df13196a9f947d00bce797
Amanda Bezerra <colmenero88@bol.com.br>
2020-10-29 15:07:13 -0300

    changing responsability limpp for ingridient version

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get '/first' => 'pages#first'
   get '/sendtous' => 'pages#sendtous'
   get '/responsible' => 'pages#responsible'
+  get '/responsibleingredient' => 'pages#responsibleingredient'
   get '/thanks' => 'produtos#thanks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
